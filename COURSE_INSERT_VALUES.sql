89b44ba9c36c3280184873580c7f02e9f0bae2eb
Jhashuva Udayagiri <joshua537.nit@gmail.com>
2017-06-19 13:22:11 +0530

    Add files via upload

---
+++ COURSE_INSERT_VALUES.sql
@@ -0,0 +1,127 @@
+insert into course values('R14HAS1101','English',4,0,4);
+
+insert into course values('R14MTH1101','Mathematics 1',4,0,4);
+
+insert into course values('R14EPH1101','Engg Physics 1',4,0,4);
+
+insert into course values('R14CSE1101','Computer Programming',4,0,4);
+
+insert into course values('R14MED1144','Engineering Drawing',4,0,4);
+
+insert into course values('R12HAS1201','English Language and Communication Skills Lab',0,3,2);
+
+insert into course values('R14CSE1201','Computer Programming Lab',0,3,2);
+
+insert into course values('R14MED1201','Workshop Practice',0,3,2);
+
+insert into course values('R14MTH1102','Mathematics 2',4,0,4);
+
+insert into course values('R14MTH1103','Numerical Methods',4,0,4);
+
+insert into course values('R14EPH1102','Engineering Physics 2',4,0,4);
+
+insert into course values('R14CSE1102','Data Structures Through C',4,1,5);
+
+insert into course values('R14ECH1101','Engineering Chemistry',4,1,5);
+
+insert into course values('R14CSE1202','Data Structures Through C Lab',0,3,2);
+
+insert into course values('R14ECH1201','Engineering Chemistry Lab',0,3,2);
+
+insert into course values('R14EPH1201','Engineering Physics 2 Lab',0,3,2);
+
+insert into course values('R14MTH1105','Probability and Statistics',4,0,4);
+
+insert into course values('R14CSE1103','Mathematical Foundations of Computer Science',4,0,4);
+
+insert into course values('R14CSE1104','Data Structures Through C++',4,0,4);
+
+insert into course values('R14ECE1136','Digital Logical Design',4,0,4);
+
+insert into course values('R14ECE1102','Electronic Devices and Circuits',4,0,4);
+
+insert into course values('R14EEE1130','Basic Electrical Engineering',4,0,4);
+
+insert into course values('R14EEE1212','Electrical and Electronics Lab',0,3,2);
+
+insert into course values('R14CSE1203','Data Structures Through C++ Lab',0,3,2);
+
+insert into course values('R14CSE1105','Computer Organization',4,0,4);
+
+insert into course values('R14CSE1106','Database Management Systems ',4,0,4);
+
+insert into course values('R14CSE1107','Java Programming',4,0,4);
+
+insert into course values('R14HAS1102','Environmental Studies',4,0,4);
+
+insert into course values('R14CSE1108','Formal Languages and Automata Theory',4,0,4);
+
+insert into course values('R14HAS1109','Design and Analysis of Algorithms',4,0,4);
+
+insert into course values('R14CSE1204','Java Programming Lab',0,3,2);
+
+insert into course values('R14CSE1205','Database Management Systems Lab ',0,3,2);
+
+insert into course values('R14HAS1110','Principles of Programming Languages',4,0,4);
+
+insert into course values('R14MTH1106','Operations Research',4,0,4);
+
+insert into course values('R14CSE1113','Software Engineering',4,0,4);
+
+insert into course values('R14CSE1118','Compiler Design',4,0,4);
+
+insert into course values('R14CSE1114','Operating Systems',4,0,4);
+
+insert into course values('R14CSE1139','Computer Networks',4,0,4);
+
+insert into course values('R14CSE1206','Operating Systems Lab',0,3,2);
+
+insert into course values('R14CSE1207','Compiler Design Lab',0,3,2);
+
+insert into course values('R14CSE1143','Distributed Systems',4,0,4);
+
+insert into course values('R14CSE1117','Information Security',4,0,4);
+
+insert into course values('R14CSE1116','Object Oriented Analysis and Design',4,0,4);
+
+insert into course values('R14CSE1121','Software Testing Methodologies',4,0,4);
+
+insert into course values('R14CSE113','Managerial Economics and Financial Analysis ',4,0,4);
+
+insert into course values('R14CSE1119','Web Technologies ',4,0,4);
+
+insert into course values('R14CSE1215','Case Tools and Web Technologies Lab ',0,3,2);
+
+insert into course values('R14HAS1202','Advanced English Language Communication Skills Lab',0,3,2);
+
+insert into course values('R14CSE1120','Linux Programming',4,0,4);
+
+insert into course values('R14CSE1128','Design Patterns',4,0,4);
+
+insert into course values('R14CSE1122','Data warehousing and Data Mining',4,0,4);
+
+insert into course values('R14CSE1125','Cloud Computing',4,0,4);
+
+insert into course values('R14CSE1127','Mobile Computing',4,0,4);
+
+insert into course values('R14CSE1130','Soft Computing',4,0,4);
+
+insert into course values('R14CSE1208','Linux Programming lab',0,3,2);
+
+insert into course values('R14CSE1216','Data Warehousing and Mining lab',0,3,2);
+
+
+insert into course values('R14HAS1104','Management Science',4,0,4);
+
+insert into course values('R14CSE1132','Web Servicse',4,0,4);
+
+insert into course values('R14CSE1138','Database Security',4,0,4);
+
+insert into course values('R14CSE1210','Industry Oriented Mini Project',0,0,2);
+
+insert into course values('R14CSE1211','Seminar',0,6,2);
+
+insert into course values('R14CSE1212','Project Work',0,15,10);
+
+insert into course values('R14CSE1213','Comprehensive Viva',0,0,2);
+
